0ce7675ae4473c587f18f3276eda50c1bd95e4e6
code-0121 <kang.chinshen@gmail.com>
2022-09-23 14:49:54 +0800

    upload from local repo

---
+++ components/index.js
@@ -0,0 +1,29 @@
+
+import HomeHeader from "./HomeHeader";
+
+import FocusedStatusBar from "./FocusedStatusBar";
+
+import NFTCard from "./NFTCard";
+
+import { RectButton, CircleButton } from "./Button";
+
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+
+import DetailsBid from "./DetailsBid";
+
+import DetailsDesc from "./DetailsDesc";
+
+export {
+    HomeHeader,
+    FocusedStatusBar,
+    NFTCard,
+    People,
+    EndDate,
+    RectButton,
+    CircleButton,
+    EthPrice,
+    NFTTitle,
+    DetailsBid,
+    DetailsDesc,
+    SubInfo
+}
